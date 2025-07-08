9c832f17deefc450106afd1dfc4d7ce5436562e6
yusufenni <yusufenni@cntbilisim.com.tr>
2025-07-08 10:54:16 +0300

    database name fixed

--- config/database.ts
+++ config/database.ts
@@ -27,9 +27,9 @@ export default ({ env }) => {
         connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        database: env('DATABASE_NAME', 'repices_db'),
+        user: env('DATABASE_USERNAME', 'repices_user'),
+        password: env('DATABASE_PASSWORD', ''),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
